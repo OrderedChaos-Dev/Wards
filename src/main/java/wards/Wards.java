c6262b3973051c092413588b7182e6903ebb6abd
mc-jt9 <modding.jt9@gmail.com>
2019-01-14 20:14:33 +0800

    DONT PUSH MEEEEE

---
+++ src/main/java/wards/Wards.java
@@ -0,0 +1,30 @@
+package wards;
+
+import org.apache.logging.log4j.Logger;
+
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.Mod.EventHandler;
+import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
+
+@Mod(modid = Wards.MODID, name = Wards.NAME, version = Wards.VERSION)
+public class Wards
+{
+    public static final String MODID = "wards";
+    public static final String NAME = "Wards";
+    public static final String VERSION = "1.0";
+
+    private static Logger logger;
+
+    @EventHandler
+    public void preInit(FMLPreInitializationEvent event)
+    {
+        logger = event.getModLog();
+    }
+
+    @EventHandler
+    public void init(FMLInitializationEvent event)
+    {
+
+    }
+}
